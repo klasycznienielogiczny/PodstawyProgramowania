29dc2825e5dcf157c5379bb05378bc3de0afe90a
klasycznienielogiczny <ppaluch@student.agh.edu.pl>
2020-11-02 21:29:31 +0100

    Poprawa cw7 - mediana

--- Laboratorium_4/cw7_mediana.c
+++ Laboratorium_4/cw7_mediana.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int sort(int tab[],int n){
-    int a,i,j;
-    for(i=1;i<n;++i){
+float mediana(int tab[], int n){
+    int a=0,j=0;
+    for(int i=n-2;i>=0;--i){
         a=tab[i];
-        for(j=i-1;j>=0;j--)
-            if(a<tab[j])
-                tab[j+1]=tab[j];
-        tab[j+1]=a;
+        j=i+1;
+        while((j<n) && (a>tab[j])){
+            tab[j-1] = tab[j];
+            j++;
+        }
+        tab[j-1]=a;
+        /*for(j=i+1;j<n;++j)
+            if(a>tab[j])
+                tab[j-1]=tab[j];
+        tab[j-2]=a;*/
     }
+    /*for(int i=0;i<n;i++)
+        printf("%d\t",tab[i]);*/
+    return n%2==0 ? 1.0*(tab[n/2-1]+tab[n/2])/2 : 1.0*tab[(n-1)/2];
 }
-
-int mediana(int tab[],int n) {
-    int tab_s[n];
-    *tab_s=sort(tab,n);
-    for (int i=0; i<n;i++)
-        printf("%d\t",tab_s[i]);
-    //return n%2==0 ? (tab_s[n/2]+tab_s[n/2+1])/2 : tab_s[(n-1/2)];
-    return 0;
-}
-
 
 int main() {
-    int tab[]={5,4,3,2,1,8};
-    //printf("Mediana rowna: %d",mediana(tab,6));
-    mediana(tab,6);
+    int tab[]={5,4,3,2,1,8,9,85};
+    //printf("L.el. %d\n",sizeof(tab)/sizeof(int));
+    printf("Mediana jest rowna %.2f",mediana(tab,sizeof(tab)/sizeof(int)));
+    //mediana(tab,sizeof(tab)/sizeof(int));
 }
--- main.c
+++ main.c
@@ -26,22 +26,6 @@
 
 
 
-/*
-#include <stdio.h>
-
-int main(){
-    int a=2, b=7;
-    printf("%d & %d = %d\n",a,b,a&b);
-    printf("%d && %d = %d\n",a,b,a&&b);
-    printf("%d | %d = %d\n",a,b,a|b);
-    printf("%d || %d = %d\n",a,b,a||b);
-    return 0;
-}
-*/
-
-
-
-
 
 
 
