2165fed0a9f4634bf672501ef37c54267e2086dc
klasycznienielogiczny <ppaluch@student.agh.edu.pl>
2020-12-28 19:18:19 +0100

    Podmiana na poprawny

--- laboratorium_10/cw8_wstaw_w_minh.c
+++ laboratorium_10/cw8_wstaw_w_minh.c
@@ -56,18 +56,18 @@ void wstaw_minh(struct drzewo *dr, int el){
     new->prawy=NULL;
     new->liczba=el;
     tmp=dr;
-    if(tmp==NULL)
+    if(dr==NULL)
         dr=new;
     else{
         while(1){
-            if(tmp->liczba > el) {
+            if(minh(tmp->prawy) > minh(tmp->lewy)) {
                 if (tmp->lewy == NULL) {
                     tmp->lewy = new;
                     break;
                 } else
                     tmp = tmp->lewy;
             }
-            if(tmp->liczba < el) {
+            if(minh(tmp->prawy)<=minh(tmp->lewy)){
                 if (tmp->prawy == NULL) {
                     tmp->prawy = new;
                     break;
@@ -93,7 +93,6 @@ void minh_sciezka(struct drzewo *dr, char sciezka[]){
 }*/
 
 int main(){
-    char sciezka[64]="";
     struct drzewo *pdr1;
     pdr1->lewy=NULL;
     pdr1->prawy=NULL;
@@ -102,9 +101,22 @@ int main(){
     wstaw(pdr1,7);
     wstaw(pdr1,0);
     printf("Min. wysokosc drzewa rowna: %d\n",minh(pdr1));
-    /*minh_sciezka(pdr1,sciezka);
+    /*char sciezka[64]="";
+    minh_sciezka(pdr1,sciezka);
     printf("Sciezka do najkrotszego: %s\n",sciezka);*/
-    printf("Wypisanie drzewa:\n");
+    printf("\nWypisanie drzewa:\n");
     wypisz(pdr1);
+    wstaw_minh(pdr1,4);
+    printf("\nWypisanie drzewa:\n");
+    wypisz(pdr1);
+    printf("\nMin. wysokosc drzewa rowna: %d\n",minh(pdr1));
+    wstaw_minh(pdr1,4);
+    printf("\nWypisanie drzewa:\n");
+    wypisz(pdr1);
+    printf("\nMin. wysokosc drzewa rowna: %d\n",minh(pdr1));
+    wstaw_minh(pdr1,1);
+    printf("\nWypisanie drzewa:\n");
+    wypisz(pdr1);
+    printf("\nMin. wysokosc drzewa rowna: %d\n",minh(pdr1));
     return 0;
 }
